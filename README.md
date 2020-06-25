eec1376f97de92cb40dc81ea5ad1d82214435c7a
Dmitry Marov <d.marov94@gmail.com>
2020-06-25 20:40:16 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to use from CLI
+edir file `lagswitch.js`
+
+node C:\\Users\dbdgod\bin\lagswitch.js
+
---
+++ lagswitch.js
@@ -0,0 +1,24 @@
+const ssfDir = "C:\\Users\\dbdgod\\bin\\ssf-win-x86_64-3.0.0";
+const configFilePath = "C:\\Users\\dbdgod\\.config\\dbd-port-scanner\\config.json"
+
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+const configStr = fs.readFileSync(configFilePath);
+const config = JSON.parse(configStr);
+const endpoint = config.endpoint;
+
+if (endpoint) {
+
+    process.chdir(ssfDir);
+    const cmd = spawn(`${ssfDir}\\ssf.exe`, ["-V", `${endpoint}:127.0.0.1:10001`, "-p", "10003", "127.0.0.1"]);
+
+    cmd.on('error', error => {
+
+        console.log(error);
+    });
+
+    setTimeout(() => {
+        cmd.kill('SIGINT');
+    }, 10000);
+}
